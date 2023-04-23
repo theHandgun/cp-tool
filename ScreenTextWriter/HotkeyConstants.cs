1c829eecb94b78f85c31d94907a84b05f747cd75
Otto Hooper <votton.hooper@gmail.com>
2023-04-23 02:45:24 +0100

    Add missing hotkey explanations

--- ScreenTextWriter/HotkeyConstants.cs
+++ ScreenTextWriter/HotkeyConstants.cs
@@ -2,15 +2,15 @@
 {
     public static class HotkeyConstants
     {
-        public const int HOTKEY_ID_1 = 9000;
-        public const int HOTKEY_ID_2 = 9001;
+        public const int HOTKEY_ID_1 = 9000; // 9000 is the ID of the first hotkey
+        public const int HOTKEY_ID_2 = 9001; // 9001 is the ID of the second hotkey
 
-        public const uint MOD_NONE = 0x0000;
+        public const uint MOD_NONE = 0x0000; //NONE
         public const uint MOD_SHIFT = 0x0004; //SHIFT
-        public const uint VK_DEL = 0x2E;
-        public const uint VK_F2 = 0x71;
-        public const uint VK_F3 = 0x72;
-        public const uint VK_F4 = 0x73;
-        public const uint VK_M = 0x4D;
+        public const uint VK_DEL = 0x2E; //DELETE
+        public const uint VK_F2 = 0x71; //F2
+        public const uint VK_F3 = 0x72; //F3
+        public const uint VK_F4 = 0x73; //F4
+        public const uint VK_M = 0x4D; //M
     }
 }
